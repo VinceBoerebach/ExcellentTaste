16d184b0a8791e7801b9f65815f9a18cf3bd03e8
arend4802 <arendclaesen@hotmail.com>
2019-11-04 14:43:09 +0100

    added first seed

--- Restaurant_Taster/Migrations/Configuration.cs
+++ Restaurant_Taster/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Restaurant_Taster.Migrations
 {
+    using Restaurant_Taster.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,6 +15,25 @@ namespace Restaurant_Taster.Migrations
 
         protected override void Seed(Restaurant_Taster.Models.ResDBContext context)
         {
+            context.MenuDB.Add(new MenuModel
+            {
+                Customer = new Customer
+                {
+                    CustomerName = "Test",
+                    Email = "Test@test.test",
+                    Phone = 0678255339
+                },
+                Food = new Food
+                {
+                    Category = "TestCategory",
+                    Description = "Testdesc",
+                    DrinkorFood = true,
+                    Price = 2.50,
+                    Tax = "9%"
+                },
+                Table = 2,
+                TimeDate = new DateTime(2019,11,11)
+            });
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 

7c432439cf6ab30b76cadf62a4e41706a2df601b
arend4802 <arendclaesen@hotmail.com>
2019-11-04 14:00:14 +0100

    admin

---
+++ Restaurant_Taster/Controllers/AdminController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant_Taster.Models;
+
+namespace Restaurant_Taster.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ResDBContext db = new ResDBContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.MenuDB.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuModel menuModel = db.MenuDB.Find(id);
+            if (menuModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuModel);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Customer,Table,TimeDate")] MenuModel menuModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MenuDB.Add(menuModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(menuModel);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuModel menuModel = db.MenuDB.Find(id);
+            if (menuModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuModel);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Customer,Table,TimeDate")] MenuModel menuModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menuModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(menuModel);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuModel menuModel = db.MenuDB.Find(id);
+            if (menuModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuModel);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MenuModel menuModel = db.MenuDB.Find(id);
+            db.MenuDB.Remove(menuModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Restaurant_Taster/Controllers/HomeController.cs
+++ Restaurant_Taster/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace Restaurant_Taster.Controllers
 
             return View();
         }
+        public ActionResult Admin()
+        {
+            string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "admin", });
+            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
+
+            return View();
+        }
     }
 }
